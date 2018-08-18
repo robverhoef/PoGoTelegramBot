f71becca15178d8138cc5bce3fd0c788a91c1ed5
Rob Verhoef <rob@romeen.nl>
2018-08-18 21:43:09 +0200

    Define association

--- models/raidboss.js
+++ models/raidboss.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   var Raidboss = sequelize.define('Raidboss', {
     name: DataTypes.STRING,
     level: DataTypes.INTEGER,
     accounts: DataTypes.STRING
-  }, {});
-  Raidboss.associate = function(models) {
+  }, {})
+  Raidboss.associate = function (models) {
     models.Raidboss.hasMany(models.Raid)
-  };
-  return Raidboss;
-};
+  }
+  return Raidboss
+}
