c7b30a253f7cf4a398f773bbe05c162b6453fd97
Rick Fleuren <fleuren@gmail.com>
2018-09-23 20:44:42 +0200

    bugje

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ bot.hears('Een gym wijzigen', Stage.enter('edit-gym-wizard'))
 bot.hears('Een raidboss toevoegen', Stage.enter('add-raidboss-wizard'))
 bot.hears('Een raidboss wijzigen', Stage.enter('edit-raidboss-wizard'))
 bot.hears('Statistieken', Stage.enter('stats-wizard'))
-bot.hears('Notificaties', Stage.enter('notification-wizard'))
+bot.hears('Notificaties van bepaalde gyms', Stage.enter('notification-wizard'))
 
 /**
 * Check if valid user and show START button to switch to private mode
