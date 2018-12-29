a5c2604c7f97b1eab54b36e8e014f00869252cc0
Rob Verhoef <rob@romeen.nl>
2018-12-29 13:30:01 +0100

    fixes #29

--- migrations/20180816092512-create-raidboss.js
+++ migrations/20180816092512-create-raidboss.js
@@ -20,12 +20,12 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('NOW()'),
+        defaultValue: Sequelize.NOW,
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('NOW()'),
+        defaultValue: Sequelize.NOW,
       }
     });
   },
