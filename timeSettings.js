8be9abb189eb6db5017fbe41fb279f14a4013f88
Rob Verhoef <rob@romeen.nl>
2019-09-05 00:32:27 +0200

    returned times back to normal

--- timeSettings.js
+++ timeSettings.js
@@ -3,11 +3,11 @@
  */
 module.exports = Object.freeze({
   // time to hatch an egg
-  HATCH: 15, // was 60
+  HATCH: 60, // was 15 on a raid hour day
   // time for raidboss to stay
-  BOSS: 90, // was 45
+  BOSS: 45, // was 90 on a raid hour day
   // default start time before the end of the raid
-  RAID_START: 30,
+  RAID_START: 15,
   // time for exraidboss to stay
   EXRAIDBOSS: 45
 })
