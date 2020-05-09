b516635588f373684be08e765f278bc1aeae7087
Vera <eddy.vera@capgemini.com>
2020-05-09 09:59:35 +0200

    Added migration model for remote raid users

---
+++ migrations/20200508181632-add_remote_column_to_raiduser.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'raidusers',
+      'remote',
+     Sequelize.BOOLEAN
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn(
+      'raidusers',
+      'remote',
+     Sequelize.BOOLEAN
+    );
+  }
+};
